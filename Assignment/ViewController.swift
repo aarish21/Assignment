961fe951ea975c9e62444db9d3f40836a44967a2
aarish21 <aarish.jerry@gmail.com>
2023-09-08 01:28:06 +0530

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Aarish on 08/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
